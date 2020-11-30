92dcfeea8563fa4bdbc0b1555d9480a9e0468037
Ivan Gonzalez <kikin-gr2000@hotmail.com>
2020-11-30 16:12:36 -0600

    Formulario-contacto fixed

--- vistas/modulos/contacto.php
+++ vistas/modulos/contacto.php
@@ -3,20 +3,20 @@
         <h2 class="fw-300 centrar-texto">
             Llena el formulario de contacto
         </h2>
-        <form action="">
+        <form class="form-contacto" action="">
             <fieldset>
                 <legend>Contacto</legend>
                 <br>
                 <label for="nombre">Nombre: </label><br>
-                <input type="text" id="nombre" placeholder="Tu nombre" class="nombre-contacto" />
+                <input type="text" id="nombre" placeholder="Tu nombre" class="nombre-contacto form-control input-lg" />
 
                 <label for="email">E-mail: </label><br>
-                <input type="email" id="email" placeholder="Tu correo" required class="correo-contacto" />
+                <input type="email" id="email" placeholder="Tu correo" required class="correo-contacto form-control input-lg" />
                 <label for="telefono">Teléfono: </label><br>
-                <input type="tel" id="telefono" placeholder="Tel. celular" class="telefono-contacto" /><br>
+                <input type="tel" id="telefono" placeholder="Tel. celular" class="telefono-contacto form-control input-lg" /><br>
                 <label for="mensaje">Mensaje:</label><br>
-                <textarea id="mensaje"></textarea>
+                <textarea id="mensaje" class="form-control"></textarea>
             </fieldset>
-            <input type="submit" class="boton boton-verde" />
+            <input type="submit" class="boton boton-verde" id="boton-contacto" />
         </form>
     </main>
