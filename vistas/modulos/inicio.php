922f63858272e61e9588b5e3e6439799b9beec55
Ivan Gonzalez <kikin-gr2000@hotmail.com>
2021-01-04 20:57:40 -0600

    slider Jalando

--- vistas/modulos/inicio.php
+++ vistas/modulos/inicio.php
@@ -3,19 +3,16 @@
         <div class="mySlides ">
             <div class="numbertext">1 / 3</div>
             <img src="vistas/img/plantilla/buds.jpg" style="width:100%">
-            <div class="text">Caption Text</div>
         </div>
 
         <div class="mySlides ">
             <div class="numbertext">2 / 3</div>
             <img src="vistas/img/plantilla/tele.jpg" style="width:100%">
-            <div class="text">Caption Two</div>
         </div>
 
         <div class="mySlides ">
             <div class="numbertext">3 / 3</div>
             <img src="vistas/img/plantilla/mi-10t.png" style="width:100%">
-            <div class="text">Caption Three</div>
         </div>
 
         <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
@@ -39,36 +36,5 @@
     </main>
 
     <script>
-        var slideIndex = 1;
-        showSlides(slideIndex);
 
-        // Next/previous controls
-        function plusSlides(n) {
-            showSlides(slideIndex += n);
-        }
-
-        // Thumbnail image controls
-        function currentSlide(n) {
-            showSlides(slideIndex = n);
-        }
-
-        function showSlides(n) {
-            var i;
-            var slides = document.getElementsByClassName("mySlides");
-            var dots = document.getElementsByClassName("dot");
-            if (n > slides.length) {
-                slideIndex = 1;
-            }
-            if (n < 1) {
-                slideIndex = slides.length;
-            }
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
-            for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" active", "");
-            }
-            slides[slideIndex - 1].style.display = "block";
-            dots[slideIndex - 1].className += " active";
-        }
     </script>
